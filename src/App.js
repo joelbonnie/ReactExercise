28a69cf257307e8f4fc70bbfe5ca649a0c82a618
Joel Bonnie <joelbcasp@gmail.com>
2023-08-27 15:54:44 -0700

    removed template code in app.js; modified app div

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const name = null;
+  const isNameShowing = false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          {2 + 3}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> hello {isNameShowing ? name : 'someone'} </h1>
+      {name ? (
+        <>
+          <h1> test </h1>
+          <h2> name is {name} </h2>
+        </>
+      ) : (
+        <>
+        <h1> test </h1>
+        <h2> there is no name </h2>
+        </>
+      )}
     </div>
   );
 }
