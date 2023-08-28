2bfee7f89cc0451892e42b999577788af86e640e
Joel Bonnie <joelbcasp@gmail.com>
2023-08-27 21:03:31 -0700

    custom components in app tutorial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
 
+const Person = (props) => {
+  return (
+    <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>Age: {props.age}</h2>
+    <br></br>
+    </>
+  )
+}
+
 const App = () => {
   const name = null;
   const isNameShowing = false;
   return (
     <div className="App">
-      <h1> hello {isNameShowing ? name : 'someone'} </h1>
-      {name ? (
-        <>
-          <h1> test </h1>
-          <h2> name is {name} </h2>
-        </>
-      ) : (
-        <>
-        <h1> test </h1>
-        <h2> there is no name </h2>
-        </>
-      )}
+      <Person 
+        name='John' 
+        lastName={'Doe'} a
+        ge={25} 
+      />
+      <Person name = 'Mary' lastName='Smith' age={20 + 10}/>
+    
+      
+      
     </div>
   );
 }
